3987269dbf2c5875f21028be0fa61e261cec658d
Alexander Sh <alexander@probegin.com>
2019-12-23 19:10:02 +0200

    wrap translatabel word

--- app/Services/Admin/resources/views/index.blade.php
+++ app/Services/Admin/resources/views/index.blade.php
@@ -11,7 +11,7 @@
     <body>
         <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
             <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ route('dashboard') }}">Artip Admin Control Panel</a>
-            <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+            <input class="form-control form-control-dark w-100" type="text" placeholder="{{ _i('Search') }}" aria-label="Search">
             <ul class="navbar-nav px-3">
                 <li class="nav-item text-nowrap">
                     <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
