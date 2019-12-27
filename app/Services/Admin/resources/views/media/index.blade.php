7258fad6508b38fbd70dcba86b022151442433af
AlexanderSh <alexander@probegin.com>
2019-12-27 15:38:56 +0200

    display media in modal

--- app/Services/Admin/resources/views/media/index.blade.php
+++ app/Services/Admin/resources/views/media/index.blade.php
@@ -18,11 +18,11 @@
             <div class="row text-center text-lg-left">
                 @foreach($collection as $file)
                 <div class="col-lg-3 col-md-4 col-6 text-center">
-                    <a href="#" data-size="{{ $file->size }}" data-ext="{{ $file->extension }}" data-mime="{{ $file->mime }}" class="d-block mb-4 h-100">
+                    <a href="{{ asset("storage/$file->path") }}" data-size="{{ $file->size }}" data-ext="{{ $file->extension }}" data-mime="{{ $file->mime }}" class="display-image-data d-block mb-4 h-100">
                         @if(in_array($file->extension, ['gif', 'jpg', 'jpeg', 'png']))
                         <img class="img-fluid img-thumbnail" src="{{ asset("storage/$file->path") }}" alt="{{ $file->name }}" title="{{ $file->name }}">
                         @else
-                        <img class="img-fluid img-thumbnail" src="holder.js/300x200?auto=yes" data-holder-rendered="true">
+                        <img class="img-fluid img-thumbnail" data-src="holder.js/300x200?auto=yes&textmode=exact" data-holder-rendered="true">
                         @endif
                     </a>
                 </div>
@@ -34,10 +34,38 @@
 <div class="mt-3">
     {{ $collection->links() }}    
 </div>
+<div class="modal fade" id="preview" tabindex="-1" role="dialog" aria-labelledby="preview" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg">        
+        <div class="modal-content">            
+            <div class="modal-header">
+                <h5 class="modal-title">{{ _i('File Preview') }}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-center">
+                <div class="container-fluid">
+                    <img src="" id="imagepreview" style="width: 400px; height: 100%;">
+                    <hr>
+                    <input id="url" type="text" class="form-control" value="">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
 @section('custom-scripts')
 <script src="{{ asset('js/holder.min.js') }}"></script>
 <script>
 $(function () {
+    $(".display-image-data").on("click", function (e) {
+        e.preventDefault();
+        $('#imagepreview').attr('src', $(this).children('img').attr('src'));
+        $('#url').attr('value', $(this).attr('href'));
+        $('#preview').modal('toggle');
+    });
     $('#file-selector').on("change", function () {
         let self = $(this),
                 formData = new FormData(),
