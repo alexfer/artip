fa30e6474178aac90d3cb01cd06f9067974b5a78
AlexanderSh <alexander@probegin.com>
2020-01-14 15:45:52 +0200

    some improvements

---
+++ app/Services/Web/resources/views/single-pages/coming-soon.blade.php
@@ -0,0 +1,9 @@
+@extends('web::index')
+@section('title', _i('Coming Soon))
+@section('content')
+<div class="card">
+    <div class="card-body h-100">
+        {{ _i('Coming Soon) }}
+    </div>
+</div>>
+@endsection
