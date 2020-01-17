6cf446fc71f065267581107880e2b97c2e8ef3de
AlexanderSh <alexander@probegin.com>
2020-01-17 11:08:49 +0200

    check extension

--- app/Services/Web/resources/views/widgets/list-news.blade.php
+++ app/Services/Web/resources/views/widgets/list-news.blade.php
@@ -5,7 +5,9 @@
             <small class="d-block my-2 text-muted">{{ date('d-m-Y', strtotime($item->created_at)) }}</small>
             <a href="{{ route('news.by.slug', ['id' => $item->id, 'slug' => $item->slug]) }}" class="d-block text-black h6 mb-3" title="{{ $item->short_title }}">{{ $item->short_title }}</a>
             @foreach($item->media as $key => $media)
-            <img class="rounded float-left mr-2 mb-1" src="{{ asset(\Storage::disk('media')->url(sprintf("50-thumb-%s", $media->file->path))) }}">
+                @if(in_array($media->file->extension, ['png', 'jpg', 'jpeg', 'gif']))
+                <img class="rounded float-left mr-2 mb-1" src="{{ asset(\Storage::disk('media')->url(sprintf("50-thumb-%s", $media->file->path))) }}">
+                @endif
             @if(!$key) @break @endif
             @endforeach
             {{ $item->long_title }}
