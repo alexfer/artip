8457a7e35b52fe06a388eed5ff5545d2ac3c664c
Alexander Sh <alexander@probegin.com>
2020-02-06 18:34:52 +0200

    change public dir

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('path.public', function() {
+            return realpath(__DIR__ . '/../');
+        });
     }
 
 }
---
+++ public_html/js/common.js
@@ -0,0 +1,30 @@
+$(function () {
+    $('#quick-message').on('submit', function (e) {
+        e.preventDefault();
+        let self = $(this);
+        self.find('.alert').hide();
+        $.post(self.attr('action'), self.serialize(), function (response) {
+            if (response.errors !== undefined) {
+                self.find('.alert-danger').html(response.errors).show();
+            } else {
+                self.find('.alert-success').html(response.message).show();
+            }
+        }, 'json');
+    });
+    $("#toTop").click(function (e) {
+        e.preventDefault();
+        $("html, body").animate({scrollTop: 0}, 1000);
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 30) {
+            $('.stycki-bar').addClass('sticky border-bottom');
+        } else {
+            $('.stycki-bar').removeClass('sticky border-bottom');
+        }
+    });
+    $('#carousel-indicators').carousel({
+        interval: 20000,
+        cycle: true
+    });
+});
