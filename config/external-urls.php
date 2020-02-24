32c5fb02aec3458133ff8b1fdc908e0ae13c15cd
Alexander Sh <alexander@probegin.com>
2020-02-24 17:20:22 +0200

    change external url

--- config/external-urls.php
+++ config/external-urls.php
@@ -3,7 +3,7 @@
 return [
     'ukc' => 'https://ukc.gov.ua',
     'vnz' => 'http://vnz.org.ua',
-    'instagram' => 'https://www.instagram.com/academrtp',
+    'instagram' => 'https://www.instagram.com/artip_lutsk',
     'youtube' => 'https://youtu.be/TEyvlRMbizc',
     'facebook' => 'https://www.facebook.com/academrtip',
     'google-plus' => env('APP_URL'),
